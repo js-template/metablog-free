dc97c26cad7f40e492e2df0766aa818df06a73fe
mdshofiur <shofiurbd13@gmail.com>
2023-03-20 15:30:28 +0600

    authorInfo color changed and imported into the page

--- apps/site/app/author/page.tsx
+++ apps/site/app/author/page.tsx
@@ -1,24 +1,23 @@
+import AuthorInfo from '@/components/authorInfo/AuthorInfo'
 import PostCard from '@/components/card/PostCard'
 import React, { Fragment } from 'react'
 
 const Author = () => {
    return (
       <Fragment>
+         {/* Author info */}
+         <AuthorInfo />
          {/* Page Body */}
-         <main className="container mx-auto">
-            <div className="my-20">
-               <h5 className="text-secondary font-bold text-2xl mb-8">
-                  Latest Post
-               </h5>
-               <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                  {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(
-                     (item: any, index: number) => (
-                        <div key={index}>
-                           <PostCard />
-                        </div>
-                     )
-                  )}
-               </div>
+         <main className="container mx-auto mt-12 mb-24 px-5 sm:px-0">
+            <h5 className="text-secondary font-bold text-2xl mb-8">
+               Latest Post
+            </h5>
+            <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+               {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item: any, index: number) => (
+                  <div key={index}>
+                     <PostCard />
+                  </div>
+               ))}
             </div>
          </main>
       </Fragment>
