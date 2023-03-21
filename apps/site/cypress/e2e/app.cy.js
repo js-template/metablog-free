66f5588c3d64e36ffbf839e5e61caac0cdb5c177
Zelal Hossain <robi@ojjomedia.com>
2023-03-21 17:05:17 +0600

    preformed all code's

--- apps/site/cypress/e2e/app.cy.js
+++ apps/site/cypress/e2e/app.cy.js
@@ -1,28 +1,28 @@
 describe('Page check', () => {
-    it('should render the home page', () => {
+   it('should render the home page', () => {
       // render the home page
       cy.visit('http://localhost:3000/')
-    })
+   })
 
-    it('navigate to the blog page', () => {
-        // Start from the home page
-        cy.visit('http://localhost:3000/')
+   it('navigate to the blog page', () => {
+      // Start from the home page
+      cy.visit('http://localhost:3000/')
 
-         // Find a link with an href attribute containing "View All Post" and click it
-        cy.contains('View All Post').click()
+      // Find a link with an href attribute containing "View All Post" and click it
+      cy.contains('View All Post').click()
 
-        // The new url should include "/blog"
-         cy.url().should('include', '/blog')
-      })
+      // The new url should include "/blog"
+      cy.url().should('include', '/blog')
+   })
 
-      it('navigate to the single-blog page from post', () => {
-        // Start from the home page
-        cy.visit('http://localhost:3000/')
+   it('navigate to the single-blog page from post', () => {
+      // Start from the home page
+      cy.visit('http://localhost:3000/')
 
-         // click the link for bog title
-         cy.get('a[href*="/single-post"]', { timeout: 15000 }).eq(3).click()
+      // click the link for bog title
+      cy.get('a[href*="/single-post"]', { timeout: 15000 }).eq(3).click()
 
-        // The new url should include "/single-post" for single blog post
-         cy.url().should('include', '/single-post')
-      })
-  })
+      // The new url should include "/single-post" for single blog post
+      cy.url().should('include', '/single-post')
+   })
+})
