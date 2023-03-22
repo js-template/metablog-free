a286c88c234ec7784b901817a26d85f2b0f60e32
Zelal Hossain <robi@ojjomedia.com>
2023-03-22 15:34:26 +0600

    New html setup added with Vite

---
+++ apps/html/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	//...
+	plugins: ['@html-eslint'],
+	overrides: [
+		{
+			files: ['*.html'],
+			parser: '@html-eslint/parser',
+			extends: ['plugin:@html-eslint/recommended'],
+		},
+	],
+}
