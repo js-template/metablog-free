1466b89d84acd3a64b79e40105561bbb638d4727
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-03-09 16:57:29 +0200

    Fixes `next/font` error: Unknown font `Source Serif Pro`

--- apps/site/app/layout.tsx
+++ apps/site/app/layout.tsx
@@ -3,13 +3,13 @@ import './globals.css'
 import React from 'react'
 import {
    Plus_Jakarta_Sans,
-   Source_Serif_Pro,
+   Source_Sans_3,
    Work_Sans,
 } from 'next/font/google'
 import { GlobalProvider } from '@/context/store'
 import Header from '@/components/organism/header'
 import Footer from '@/components/organism/footer'
-import { Providers } from '@/utils/themeMode' // Plus Jakarta Sans font family with 4 weights and 2 styles
+import { Providers } from '@/utils/themeMode'
 
 // Plus Jakarta Sans font family with 4 weights and 2 styles
 const Jakarta_Sans = Plus_Jakarta_Sans({
@@ -29,13 +29,13 @@ const work_Sans = Work_Sans({
    variable: '--font-work-sans',
 })
 
-// Source Serif Pro font family with 4 weights and 2 styles
-const source_Serif_Pro = Source_Serif_Pro({
+// Source Sans 3 font family with 4 weights and 2 styles
+const source_Sans_3 = Source_Sans_3({
    weight: ['200', '300', '400', '600', '700'],
    style: ['normal', 'italic'],
    subsets: ['latin'],
    display: 'swap',
-   variable: '--font-source-serif-pro',
+   variable: '--font-source-sans-3',
 })
 
 export default function RootLayout({
@@ -47,7 +47,7 @@ export default function RootLayout({
       <html
          lang="en"
          suppressHydrationWarning
-         className={`${source_Serif_Pro.variable} ${Jakarta_Sans.variable} ${work_Sans.variable} font-sans`}
+         className={`${source_Sans_3.variable} ${Jakarta_Sans.variable} ${work_Sans.variable} font-sans`}
       >
          <body>
             <Providers>
