5995a4aaab0f12e3bfcd9dfeaa523d9ef8cf1221
Melanie Shepherd <mommaroodles@users.noreply.github.com>
2025-03-09 17:44:27 +0200

    fix Unrecognized key(s) in object: 'appDir' at "experimental"

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    experimental: {
-      appDir: true,
-   },
+     },
    // image optimization
    images: {
       domains: ['placehold.co', 'via.placeholder.com'],
